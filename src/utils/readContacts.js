1760cd835b38a2795f6e7ab044130bdd2f6afe93
AnnyCherkashina <149014624+AnnyCherkashina@users.noreply.github.com>
2025-06-15 15:02:53 +0300

    update nodejs

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,9 @@
+import { readFile } from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+const readContacts = async () => {
+    const data = await readFile(PATH_DB, 'utf-8');
+    return JSON.parse(data);
+};
+
+export default readContacts;
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,8 @@
+import { writeFile } from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+const writeContacts = async (contacts) => {
+    await writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+};
+
+export default writeContacts;
